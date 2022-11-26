27578c3e4c9c0aee1cefb3f629027352db2e6377
AnilBamne <anilbamne20@gmail.com>
2022-11-26 08:34:59 +0530

    Adding UC3EditExistingContact code

--- AddressBookSystem222Batch/AddressBookBuilder.cs
+++ AddressBookSystem222Batch/AddressBookBuilder.cs
@@ -17,13 +17,16 @@ namespace AddressBookSystem222Batch
         {
             this.ContactsList = new List<Contact>();
         }
+        //Method for adding contacts to list by taking input from user
         public void AddContacts(String firstName, String lastName, String address, String city, String state, String zip, String phone, String email)
         {
             Contact contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
             this.ContactsList.Add(contact);
         }
+        //DisplayContact method used for displaying contact list
         public void DisplayContact()
         {
+            Console.WriteLine("----  Displaying Contacts ----\n");
             foreach(Contact contact in this.ContactsList)
             {
                 Console.WriteLine("First Name = "+contact.FirstName);
@@ -34,6 +37,60 @@ namespace AddressBookSystem222Batch
                 Console.WriteLine("Zip code  = "+contact.Zip);
                 Console.WriteLine("Phone number  = "+contact.PhoneNum);
                 Console.WriteLine("Email ID  = "+contact.Email);
+            }
+        }
+        //EditExistingContact method used for editin the existing contact details
+        public void EditExistingContact()
+        {
+            search:
+            Console.WriteLine("\nEnter first name of the person for editing contact");
+            String firstname=Console.ReadLine();
+            foreach( Contact contact in this.ContactsList)      //Iterating contacts iin list
+            {
+                //Checking the existatnce of the person name in contact list
+                if (contact.FirstName == firstname)
+                {
+                    ReCheck:
+                    Console.WriteLine("Please select the area of editing \n" +
+                        "1)First Name\n2)Last Name\n3)Address\n4)Phone Number\n5)Email_Id");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Please enter your first name : ");
+                            contact.FirstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Please enter your last name : ");
+                            contact.LastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Please enter your Address : ");
+                            contact.Address = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Please enter your Phone Number : ");
+                            contact.PhoneNum = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Please enter your email Id: ");
+                            contact.Email = Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("Entered an Invalid input!");
+                            Console.WriteLine("Do you want to try again ");
+                            if (Console.ReadKey().Key == ConsoleKey.Y)
+                            {
+                                goto ReCheck;
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("*** Wrong Input ***: please enter a valid First name");
+                    goto search; 
+                }
             }
         }
     }
